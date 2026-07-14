69c69863d115dbc714d12bbccc46bd92ea00c144
liangzhili <123@qq.com>
2026-07-14 16:23:20 +0800

    修改网站icon

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -56,7 +56,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/favicon.ico",
+			src: "/favicon/favicon-pasule.ico",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
